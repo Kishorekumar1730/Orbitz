da0881b129ef2baefd7e8c69cba05c4c5dd7d207
imran.s.s <imranss1309@gmail.com>
2025-08-24 12:00:49 +0530

    Initial commit

---
+++ client/src/components/sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const sidebar = () => {
+  return (
+    <div>
+        <h1>This is a sidebar</h1>
+        </div>
+  )
+}
+export default sidebar
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import React, { use } from 'react'
+import Sidebar from '../components/sidebar'
+import { Outlet } from 'react-router-dom'
 const Layout = () => {
+  const[sidebarOpen, setSidebarOpen] = useState(false)
   return (
-    <div>Layout</div>
+    <div className='w-full flex h-screen '>
+      <sidebar />
+      <div className='flex-1 bg-slate-50'>
+        <outlet />
+
+      </div>
+      <h1>Layout</h1>
+      </div>
   )
 }
 
